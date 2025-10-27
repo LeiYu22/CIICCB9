fa472a13483318de2302e350a8fab63c2d50116d
Levy Abling <levyabling@gmail.com>
2025-10-27 12:09:06 +0800

    Create Task2.java

---
+++ Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte b = 3;
+        short s = 11;
+        int i = 0;
+        float f = 2.0f;
+        char c1 = 'H';
+        char c2 = 'w';
+        boolean bool = true;
+        String output = "" + c1 + s + i + i + c2 + i + "r" + s + "d " + f + " " + bool;
+
+        System.out.println(output);
+      
