d02e0d775fc5cfb037b8379152341619fcb87607
Levy Abling <levyabling@gmail.com>
2025-11-28 13:18:48 +0800

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,15 +1,19 @@
-public class Task2 {
+public class Main {
     public static void main(String[] args) {
-        byte b = 3;
-        short s = 11;
-        int i = 0;
-        float f = 2.0f;
-        char c1 = 'H';
-        char c2 = 'w';
-        boolean bool = true;
-        String output = "" + c1 + s + i + i + c2 + i + "r" + s + "d " + f + " " + bool;
+
+        byte three = 3;        // byte
+        short zero = 0;        // short
+        int one = 1;           // int
+        float twoPointZero = 2.0f; // float
+        char H = 'H';          // char
+        char l = 'l';          // char
+        boolean t = true;      // boolean
+
+        String output = "" 
+                + H + three + l + l + zero 
+                + " w" + zero + "r" + one + "d "
+                + twoPointZero + " " + t;
 
         System.out.println(output);
-        
-      
-        
+    }
+}
